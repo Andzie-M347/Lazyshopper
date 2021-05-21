d53f41874a931022f8b087ca3e9e5f388e8a12ef
Andzie-M347 <andzieshine@gmail.com>
2021-05-21 04:24:39 +0200

    Worked on footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,22 @@
 
 // alert('hello world')
 // Newsletter Modal
-// let newsLetter = document.querySelector('.newsletter')
-// let closeBtn = document.querySelector('.close-btn')
+let newsLetter = document.querySelector('.newsletter')
+let closeBtn = document.querySelector('.close-btn')
 
-// window.addEventListener('load', function () {
-//     var newsLetterPopup = function () {
-//         newsLetter.classList.toggle('active')
-//     };
+window.addEventListener('load', function () {
+    var newsLetterPopup = function () {
+        newsLetter.classList.toggle('active')
+    };
     
-//     setTimeout(newsLetterPopup, 8000);
-// })
+    setTimeout(newsLetterPopup, 8000);
+})
 
-// closeBtn.addEventListener('click', function() {
-//     newsLetter.classList.remove('active')
-// })
+closeBtn.addEventListener('click', function() {
+    newsLetter.classList.remove('active')
+})
 
 
+// Copyrights 
+let copyrightDate = document.querySelector('#copyright')
+copyrightDate.appendChild(document.createTextNode(new Date().getFullYear()))
