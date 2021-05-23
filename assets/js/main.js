88501c61e4e069900bddb7128277508e05a2fe66
Andzie-M347 <andzieshine@gmail.com>
2021-05-24 01:38:27 +0200

    Added sticky navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 // alert('hello world')
 // Newsletter Modal
-let newsLetter = document.querySelector('.newsletter')
-let closeBtn = document.querySelector('.close-btn')
+// let newsLetter = document.querySelector('.newsletter')
+// let closeBtn = document.querySelector('.close-btn')
 
-window.addEventListener('load', function () {
-    var newsLetterPopup = function () {
-        newsLetter.classList.toggle('active')
-    };
+// window.addEventListener('load', function () {
+//     var newsLetterPopup = function () {
+//         newsLetter.classList.toggle('active')
+//     };
 
-    setTimeout(newsLetterPopup, 8000);
-})
+//     setTimeout(newsLetterPopup, 8000);
+// })
 
-closeBtn.addEventListener('click', function() {
-    newsLetter.classList.remove('active')
-})
+// closeBtn.addEventListener('click', function() {
+//     newsLetter.classList.remove('active')
+// })
 
 
 // Copyrights 
@@ -50,3 +50,9 @@ prev.innerHTML = `&larr;`
 
 let next = document.body.querySelector('[data-controls=next]')
 next.innerHTML = `&rarr;`
+
+// Sticky Header 
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.site-header')
+    header.classList.toggle('fixed', window.scrollY > 0)
+})
