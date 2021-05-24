c2be0f579844316c62c2b67d4b0aa11451bfd38e
Andzie-M347 <andzieshine@gmail.com>
2021-05-24 04:53:20 +0200

    Added scroll top feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,29 @@ window.addEventListener('scroll', function() {
     var header = document.querySelector('.site-header')
     header.classList.toggle('fixed', window.scrollY > 0)
 })
+
+// Scroll to top
+document.addEventListener("scroll", handleScroll);
+var scrollToTopBtn = document.querySelector(".scroll-top");
+
+function handleScroll() {
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.5;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+    if(!scrollToTopBtn.classList.contains("showScrollBtn"))
+    scrollToTopBtn.classList.add("showScrollBtn")
+  } else {
+    if(scrollToTopBtn.classList.contains("showScrollBtn"))
+    scrollToTopBtn.classList.remove("showScrollBtn")
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
